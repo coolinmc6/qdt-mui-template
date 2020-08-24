99580560a5ea63bf76cca8782deaf9316df62f8d
Colin McNamara <coolinmc6@gmail.com>
2020-08-24 10:34:35 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Qdt-Components and Material UI Starter
+
+```sh
+# clone the repo and go to it
+npm i
+npm run dev
+```
+
+- The `<MyChart />` component is an example, you can get rid of it.
+- Update the `config` directory with the configuration for your app
---
+++ src/MyChart.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef } from 'react';
+import { qdtCompose, QdtPicasso, useBarChartSettings, usePieChartSettings, QdtButton } from 'qdt-components';
+import appPromise from './config/app';
+import { makeStyles } from '@material-ui/core/styles';
+
+const hypercube = {
+  qInfo: { qId: 'Sales by Year', qType: 'data'},
+  qHyperCubeDef: {
+    qDimensions: [
+      // { qDef: { qFieldDefs: ['[Country]']} },
+      { qDef: { qFieldDefs: ['[Product Group Desc]']} }
+    ],
+    qMeasures: [
+      { qDef: { qDef: 'SUM([Sales Margin Amount])'}, },
+    ],
+    qInitialDataFetch: [{
+      qTop: 0, qLeft: 0, qWidth: 10, qHeight: 1000,
+    }],
+    qInterColumnSortOrder: [],
+    qSuppressZero: true,
+    qSuppressMissing: true,
+  }
+}
+
+const myChartStyles = makeStyles((theme) => ({
+  root: {
+    position: 'relative',
+  }
+}))
+
+const MyChart = () => {
+  const barChart = useRef(null);
+  const pieChart = useRef(null);
+  const clearButton = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const settings = useBarChartSettings();
+      const app = await appPromise;
+      qdtCompose({
+        app,
+        element: barChart.current,
+        component: QdtPicasso,
+        options: {
+          settings,
+        },
+        properties: hypercube,
+        loading: () => null,
+      })
+
+      const pieSettings = usePieChartSettings();
+      qdtCompose({
+        app,
+        element: pieChart.current,
+        component: QdtPicasso,
+        options: {
+          settings: pieSettings,
+        },
+        properties: hypercube,
+        loading: () => null,
+      })
+
+      qdtCompose({
+        app, 
+        element: clearButton.current, 
+        component: QdtButton,
+        options: {
+          type: 'clearSelections',
+          label: 'Clear Selections'
+        }
+      })
+    })()
+  }, [])
+
+  const classes = myChartStyles();
+  return (
+    <div className={classes.root}>
+      <h2>My Charts</h2>
+      <div ref={clearButton} style={{ margin: '10px auto', width: 250 }} />
+      <div ref={barChart} style={{ height: 400, paddingTop: 50 }} />
+      <div ref={pieChart} style={{ height: 400, paddingTop: 50 }} />
+    </div>
+  )
+}
+
+export default MyChart;
