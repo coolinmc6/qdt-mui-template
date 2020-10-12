c61ca09285cf05f9c98cbfddaeccdf3714403bfc
Colin McNamara <coolinmc6@gmail.com>
2020-10-12 16:52:44 -0400

    Add async capability

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ import './css/styles.scss';
 import MyChart from './MyChart';
 
 const App = () => {
-    
+  (async () => {
+    const apple = new Promise((res) => {
+      setTimeout(() => {
+        res('apple');
+      }, 3000)
+    })
+
+    const _apple = await apple;
+    console.log(_apple);
+  })()
   return (
     <div>
       <h1>Qdt-Components 3.0 Tutorial</h1>
